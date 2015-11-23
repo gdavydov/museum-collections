de19bac45f4c321bbccc53ad20ff404c8004d79a
gdavydov <gdavydov@gmail.com>
2015-11-23 16:21:46 -0600

    Brought back title header_home

--- collection-management-system/share-amp/src/main/amp/config/alfresco/site-webscripts/org/alfresco/components/share/ucm-header/share-header.get.js
+++ collection-management-system/share-amp/src/main/amp/config/alfresco/site-webscripts/org/alfresco/components/share/ucm-header/share-header.get.js
@@ -1,7 +1,7 @@
 <import resource="classpath:/alfresco/site-webscripts/org/alfresco/share/imports/share-header.lib.js">
 
 if (!user.isAdmin) {
-	widgetUtils.deleteObjectFromArray(model.jsonModel, "id", "HEADER_HOME");
+//	widgetUtils.deleteObjectFromArray(model.jsonModel, "id", "HEADER_HOME");
 	widgetUtils.deleteObjectFromArray(model.jsonModel, "id", "HEADER_MY_FILES");
 	widgetUtils.deleteObjectFromArray(model.jsonModel, "id", "HEADER_SHARED_FILES");
 	widgetUtils.deleteObjectFromArray(model.jsonModel, "id", "HEADER_TASKS");
