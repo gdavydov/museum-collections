269b1bd6077b1b6e87efc7af30df5243a7c0c818
DmitryAlexandrov <elmer.ua@gmail.com>
2016-03-20 01:56:50 +0200

    Add missed file Remove webscripts used to benchmark site size calculation. Remove unused implementation of site size report in form of .xls file, containing html table (it works, but we have webscript generating proper XLS).

--- collection-management-system/repo-amp/src/main/amp/config/alfresco/extension/templates/webscripts/com/alfresco/museum/ucm/perf-test/perf-test-lucene.get.js
+++
@@ -1,9 +0,0 @@
-function main() {
-	var results = search.luceneSearch('PATH:"/app:company_home/st:sites/cm:museum-test-site/cm:perf-test-temporary-folder//*" AND +TYPE:"cm:content"');
-	var size = 0;
-	for (i = 0; i < results.length; ++i) {
-		size += results[i].size;
-	}
-	model.size = size;
-}
-main();
