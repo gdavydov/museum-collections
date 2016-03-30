7f8e6188b95f67b2b7d12777ba1fe3a6e2dcfda2
DmitryAlexandrov <elmer.ua@gmail.com>
2016-03-30 16:50:33 +0300

    Update AMPs

--- dev-scripts/amps_build.bat
+++ dev-scripts/amps_build.bat
@@ -2,10 +2,10 @@ call %~dp0\env.bat
 
 pushd %SCRIPTPATH%\..\collection-management-system
 	pushd repo-amp
-		call mvn package && copy /Y target\collection-management-system-repo-amp.amp ..\..\release_amps\
+		call mvn clean && call mvn package && copy /Y target\collection-management-system-repo-amp.amp ..\..\release_amps\
 	popd
 	pushd share-amp
-		call mvn package && copy /Y target\collection-management-system-share-amp.amp ..\..\release_amps\
+		call mvn clean && call mvn package && copy /Y target\collection-management-system-share-amp.amp ..\..\release_amps\
 	popd
 popd
 pause
